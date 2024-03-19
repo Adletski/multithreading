6015b7372c6037eccd3d33bb989828f7801fc530
Adlet Zhantassov <Adlet@MacBook-Pro-Techman.local>
2024-03-19 18:01:43 +0600

    init

---
+++ multithreading/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  multithreading
+//
+//  Created by Adlet Zhantassov on 19.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
